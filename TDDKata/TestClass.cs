bfce4598a53b12afd9e51a4f47051dca507d16be
mamontovcs <vintixfish@gmail.com>
2021-05-11 13:04:31 +0300

    Added few tests for TDD

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -1,20 +1,51 @@
-﻿// NUnit 3 tests
-// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace TDDKata
 {
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void SimpleTest()
+        private StringCalc stringCalc;
+
+        [SetUp]
+        public void SetUp()
         {
-            StringCalc calc = new StringCalc();
-            int value = calc.Sum("2,2");
+            stringCalc = new StringCalc();
+        }
+
+        [Test]
+        public void Sum_TwoPositiveDigits_CorrectResult()
+        {
+            int value = stringCalc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+        }
+
+        [Test]
+        public void Sum_OneDigitIsEmptyString_CorrectResult()
+        {
+            int value = stringCalc.Sum(" ,2");
+            Assert.That(value, Is.EqualTo(2), "Wrong actual value");
+        }
+
+        [Test]
+        public void Sum_OneDigit_CorrectResult()
+        {
+            int value = stringCalc.Sum("1");
+            Assert.That(value, Is.EqualTo(1), "Wrong actual value");
+        }
+
+        [Test]
+        public void Sum_OneDigitIsNegative_CorrectResult()
+        {
+            int value = stringCalc.Sum("1,-4");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void Sum_ToMuchArguments_CorrectResult()
+        {
+            int value = stringCalc.Sum("1,1,1,1,1");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
     }
 }
