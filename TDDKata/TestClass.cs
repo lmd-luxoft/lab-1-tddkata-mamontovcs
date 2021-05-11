4144e0c390b48f944df3b99c171b56f0a86e8fdf
mamontovcs <vintixfish@gmail.com>
2021-05-11 13:53:25 +0300

    Added tests for more complicated format

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -54,5 +54,24 @@ namespace TDDKata
             int value = stringCalc.Sum("1\n4\n4,4,4\n4\n4");
             Assert.That(value, Is.EqualTo(25), "Wrong actual value");
         }
+
+        [Test]
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//;\n1;2;3;4;5:6:7;", 28)]
+        public void Sum_PrefixExists_CorrectResult(string str, int expectedResult)
+        {
+            int value = stringCalc.Sum(str);
+            Assert.That(value, Is.EqualTo(expectedResult), "Wrong actual value");
+        }
+
+        [Test]
+        [TestCase(";\n1;2", -1)]
+        [TestCase(";\n1:2;3;4;5:6;7;", -1)]
+        [TestCase(",\n1,2,3,4\n5,6,7", 28)]
+        public void Sum_PrefixDoesNotExist_CorrectResult(string str, int expectedResult)
+        {
+            int value = stringCalc.Sum(str);
+            Assert.That(value, Is.EqualTo(expectedResult), "Wrong actual value");
+        }
     }
 }
